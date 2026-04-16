165119692f027dd231f93c4a4fb13da735bc9bec
MuhammadIbtisam <ibtisam@elgrocer.com>
2026-04-16 20:08:14 +0100

    -- Implement soft delete for animals and veterinarians

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,9 @@
 class Animal < ApplicationRecord
   has_many :tests, dependent: :restrict_with_exception
+
+  def destroy
+    raise ActiveRecord::DeleteRestrictionError.new(:tests) if tests.any?
+    update(deleted_at: Time.current)
+  end
+
 end
