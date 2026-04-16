ad196cc6b4f30d550002b84d9e117d32088f4a47
MuhammadIbtisam <ibtisam@elgrocer.com>
2026-04-16 19:29:28 +0100

    -- Add uniqueness validation to animal unique_tag

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
   has_many :tests, dependent: :restrict_with_exception
+  # As Animal tags are case insensitive identifiers. ABC123 and abc123 are the same tag.
+  validates :unique_tag, uniqueness: { case_sensitive: false }
 end
--- test/models/animal_test.rb
+++ test/models/animal_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class AnimalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'is invalid with a duplicate unique_tag' do
+    Animal.create!(unique_tag: 'ABC123', species: 'Cattle', breed: 'Angus')
+    duplicate = Animal.new(unique_tag: 'ABC123', species: 'Cattle', breed: 'Angus')
+    assert duplicate.invalid?
+    assert_includes duplicate.errors[:unique_tag], 'has already been taken'
+  end
+
+  test 'is invalid with a duplicate unique_tag case insensitively' do
+    Animal.create!(unique_tag: 'ABC123', species: 'Cattle', breed: 'Angus')
+    duplicate = Animal.new(unique_tag: 'abc123', species: 'Cattle', breed: 'Angus')
+    assert duplicate.invalid?
+    assert_includes duplicate.errors[:unique_tag], 'has already been taken'
+  end
+
 end
